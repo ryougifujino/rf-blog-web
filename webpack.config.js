602236ae940456f461b8cb3c74c21b862b3f443c
ryougifujino <ryougifujino@gmail.com>
2019-03-14 13:56:08 +0800

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,40 @@
 const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: "development",
+    devtool: "inline-source-map",
     entry: {
-        app: './src/entry.js'
+        app: './src/main.js'
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: "[name].bundle.js"
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            },
+            {
+                test: /\.(jpg|jpeg|gif|svg|png)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: "rf's Blog",
+            template: "./index.html"
+        })
+    ]
 };
