309afe018e60a22cc2f3b05695486bd1c92c4b4b
ryougifujino <ryougifujino@gmail.com>
2019-03-27 23:53:19 +0800

    Add basic markdown editor

--- src/views/PostEdit.vue
+++ src/views/PostEdit.vue
@@ -1,24 +1,49 @@
 <template>
     <div class="post-edit">
         <div class="post-edit__toolbar">
-            <input class="post-edit__title">
+            <input class="post-edit__title" spellcheck="false">
         </div>
-        <div class="post-edit__markdown-input">
-
-        </div>
-        <div class="post-edit__markdown-preview">
-
-        </div>
+        <article class="post-edit__markdown-input">
+            <textarea v-model="markdownInput" spellcheck="false"></textarea>
+        </article>
+        <article class="post-edit__markdown-preview markdown-body" v-html="markdownPreview">
+        </article>
     </div>
 </template>
 
 <script>
+    import hljs from 'highlight.js';
+    import 'highlight.js/styles/atom-one-light.css';
 
-    export default {}
+    const md = require('markdown-it')({
+        highlight(str, lang) {
+            if (lang && hljs.getLanguage(lang)) {
+                try {
+                    return hljs.highlight(lang, str).value;
+                } catch (__) {
+                }
+            }
+            return ''; // use external default escaping
+        }
+    });
+
+    export default {
+        data() {
+            return {
+                markdownInput: ''
+            };
+        },
+        computed: {
+            markdownPreview() {
+                return md.render(this.markdownInput);
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
     @import "~@/assets/styles/theme";
+    @import "~github-markdown-css";
 
     .post-edit {
         * {
@@ -26,6 +51,7 @@
         }
 
         height: 100vh;
+        overflow: hidden; // otherwise textarea will expand its container div
         $toolbar-height: 64px;
 
         &__toolbar {
@@ -51,10 +77,30 @@
         &__markdown-input {
             @extend %markdown;
             border-right: 1px solid $color-accent-dark;
+
+            > textarea {
+                height: 100%;
+                width: 100%;
+                border: none;
+                outline: none;
+                resize: none;
+                padding: 16px;
+            }
         }
 
         &__markdown-preview {
             @extend %markdown;
+            overflow: auto;
+            padding: 16px;
+
+            pre, span {
+                background-color: $text-color-secondary-light3;
+            }
+
+            img {
+                background-color: $color-accent;
+            }
         }
+
     }
 </style>
