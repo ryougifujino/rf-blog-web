b41a9352b84bb96e43c32b03ebc299d8f9d6e228
ryougifujino <ryougifujino@gmail.com>
2019-03-27 16:36:44 +0800

    Add basic post edit layout

--- src/views/PostEdit.vue
+++ src/views/PostEdit.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        PostEdit
+    <div class="post-edit">
+        <div class="post-edit__toolbar">
+            <input class="post-edit__title">
+        </div>
+        <div class="post-edit__markdown-input">
+
+        </div>
+        <div class="post-edit__markdown-preview">
+
+        </div>
     </div>
 </template>
 
@@ -9,6 +17,44 @@
     export default {}
 </script>
 
-<style>
+<style lang="scss">
+    @import "~@/assets/styles/theme";
 
+    .post-edit {
+        * {
+            box-sizing: border-box;
+        }
+
+        height: 100vh;
+        $toolbar-height: 64px;
+
+        &__toolbar {
+            padding: 16px;
+            height: $toolbar-height;
+            border-bottom: 1px solid $color-accent-dark;
+        }
+
+        &__title {
+            border: none;
+            outline: none;
+            font-weight: bold;
+            font-size: 24px;
+            line-height: 32px;
+        }
+
+        %markdown {
+            height: calc(100% - #{$toolbar-height});
+            width: 50%;
+            float: left;
+        }
+
+        &__markdown-input {
+            @extend %markdown;
+            border-right: 1px solid $color-accent-dark;
+        }
+
+        &__markdown-preview {
+            @extend %markdown;
+        }
+    }
 </style>
