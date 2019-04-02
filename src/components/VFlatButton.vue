c6f2051c0c51d7ffdf3604fdd1d5aeca6b291a92
ryougifujino <ryougifujino@gmail.com>
2019-04-03 00:29:58 +0800

    Register base components globally

--- src/components/VFlatButton.vue
+++ src/components/VFlatButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flat-button">
+    <div class="v-flat-button">
         <slot></slot>
     </div>
 </template>
@@ -7,16 +7,17 @@
 <style lang="scss">
     @import "~@/assets/styles/theme";
 
-    .flat-button {
+    .v-flat-button {
         display: inline-block;
         padding: 8px 16px;
         border-radius: 6px;
         background-color: $text-color-secondary-light2;
         color: $text-color-secondary;
         font-weight: bold;
+        user-select: none;
     }
 
-    .flat-button:hover {
+    .v-flat-button:hover {
         cursor: pointer;
         background-color: $text-color-secondary-light;
         color: $text-color-primary-light;
