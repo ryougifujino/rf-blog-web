29af22db3903595cfaa8351f2b14f779a6d8ddae
ryougifujino <ryougifujino@gmail.com>
2019-04-12 14:00:57 +0800

    Add tag component

---
+++ src/components/VTag.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="v-tag">
+        <slot></slot>
+    </div>
+</template>
+
+<style lang="scss">
+    @import "~@/assets/styles/theme";
+
+    .v-tag {
+        display: inline-block;
+        padding: 8px 16px;
+        border-radius: 16px;
+        border: 1px solid $text-color-secondary;
+        color: $text-color-secondary;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+        max-width: 200px;
+        overflow: hidden;
+
+
+        &:hover {
+            cursor: pointer;
+            border: 1px solid $text-color-primary-light;
+            color: $text-color-primary-light;
+        }
+    }
+</style>
