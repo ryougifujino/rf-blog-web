142ff231de5b509de7c0590fdbe5ad0415f3e1cb
ryougifujino <ryougifujino@gmail.com>
2019-04-18 14:13:58 +0800

    Update progress bar

--- src/components/VProgressBar.vue
+++ src/components/VProgressBar.vue
@@ -1,16 +1,54 @@
 <template>
     <div :class="['region-mask', {'region-mask--transparent': isTransparent}]">
-        <img src="../assets/images/progress-bar.svg" class="v-progress-bar">
+        <svg xmlns="http://www.w3.org/2000/svg"
+             class="v-progress-bar"
+             viewBox="0 0 100 100"
+             :width="baseSize * scale"
+             :height="baseSize * scale"
+             preserveAspectRatio="xMidYMid">
+            <circle cx="50"
+                    cy="50"
+                    fill="none"
+                    :stroke="strokeColor"
+                    stroke-width="10"
+                    r="35"
+                    stroke-dasharray="164.93361431346415 56.97787143782138"
+                    transform="rotate(227.962 50 50)">
+                <animateTransform attributeName="transform"
+                                  type="rotate"
+                                  calcMode="linear"
+                                  values="0 50 50;360 50 50"
+                                  keyTimes="0;1"
+                                  dur="1s"
+                                  begin="0s"
+                                  repeatCount="indefinite">
+                </animateTransform>
+            </circle>
+        </svg>
     </div>
 </template>
 
 <script>
     export default {
+        props: {
+            scale: {
+                type: Number,
+                default: 1
+            }
+        },
         data: () => ({
-            isTransparent: false
+            isTransparent: false,
+            isColored: false,
+            baseSize: 50
         }),
+        computed: {
+            strokeColor() {
+                return this.isColored ? "#2196f3" : "#ffffff";
+            }
+        },
         mounted() {
-            this.isTransparent = typeof this.$attrs['transparent'] === 'string';
+            this.isTransparent = 'transparent' in this.$attrs;
+            this.isColored = 'colored' in this.$attrs;
         }
     }
 </script>
@@ -21,7 +59,5 @@
     .v-progress-bar {
         @extend %center;
         background: #ff000000;
-        width: 50px;
-        height: 50px;
     }
 </style>
