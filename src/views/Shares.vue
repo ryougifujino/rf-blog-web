fbe3adf6fcdf98cdcca915e4480bc9ee87b64281
ryougifujino <ryougifujino@gmail.com>
2019-04-23 00:02:10 +0800

    Update shares layout

--- src/views/Shares.vue
+++ src/views/Shares.vue
@@ -1,21 +1,45 @@
 <template>
     <div class="shares">
-        <ShareList/>
+        <div class="shares__add-new" @click="isShowPublisher = true">新增分享</div>
+        <SharesList></SharesList>
+        <SharesPublisher :visible.sync="isShowPublisher"></SharesPublisher>
     </div>
 </template>
 
 <script>
-    import ShareList from "@/components/ShareList.vue";
+    import SharesList from "@/components/SharesList.vue";
+    import SharesPublisher from "@/components/SharesPublisher.vue";
 
     export default {
         components: {
-            ShareList
-        }
+            SharesList,
+            SharesPublisher
+        },
+        data: () => ({
+            isShowPublisher: false
+        })
     }
 </script>
 
-<style>
-    .shares{
+<style lang="scss">
+    @import "~@/assets/styles/theme";
+
+    .shares {
         padding: 0 16px 16px;
+
+        &__add-new {
+            padding: 8px 16px;
+            border-radius: 16px;
+            border: 1px solid $color-accent-dark;
+            background: $color-accent-dark;
+            margin-top: 16px;
+            text-align: center;
+
+            &:hover {
+                cursor: pointer;
+                user-select: none;
+                border: 1px solid $text-color-secondary;
+            }
+        }
     }
 </style>
