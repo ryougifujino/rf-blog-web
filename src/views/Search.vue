120335ba82ff69d6c2dd5bfd8b448c99286ffc63
ryougifujino <ryougifujino@gmail.com>
2019-05-05 14:59:31 +0800

    Add search logic

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,28 +3,73 @@
         <div class="search__header">
             <div class="search__input-container">
                 <VIcon name="baseline-search-24px"></VIcon>
-                <input spellcheck="false" class="search__input">
+                <input v-model="searchKey" spellcheck="false" class="search__input">
             </div>
         </div>
         <div class="search__result">
-            <div class="search__result-column">
+            <div class="search__result-column" v-if="filteredPosts.length">
                 <h2 class="search__result-title">博文</h2>
-                <a class="search__result-post"></a>
+                <router-link class="search__result-post"
+                             v-for="post of filteredPosts"
+                             :key="post.id"
+                             :to="'/post/' + post.id">{{post.title}}
+                </router-link>
             </div>
-            <div class="search__result-column">
+            <div class="search__result-column" v-if="filteredShares.length">
                 <h2 class="search__result-title">分享</h2>
-                <a class="search__result-share"></a>
+                <a class="search__result-share"
+                   v-for="share of filteredShares"
+                   :key="share.id"
+                   @click.prevent="openShareLink(share.url)">{{share.title}}
+                </a>
             </div>
         </div>
+        <VProgressBar v-if="isLoading" colored transparent></VProgressBar>
     </div>
 </template>
 
 <script>
+    import {mapActions, mapState} from "vuex";
+    import {mapModuleState} from "@/util/mapStateUtils";
+    import {FETCH_POST_TITLES, FETCH_SHARES} from "@/store/action-types";
+
+    const RE_URL = /^https?:\/\//;
+    const RESULT_LIMIT = 20;
     export default {
+        data: () => ({
+            searchKey: '',
+            isLoading: true
+        }),
+        computed: {
+            ...mapState(['posts']),
+            ...mapModuleState('shares', ['shares']),
+            searchKeyRE() {
+                return new RegExp(this.searchKey.trim(), 'i');
+            },
+            filteredPosts() {
+                return this.posts.filter(({title}) => this.searchKeyRE.test(title)).slice(0, RESULT_LIMIT);
+            },
+            filteredShares() {
+                return this.shares.filter(({title}) => this.searchKeyRE.test(title)).slice(0, RESULT_LIMIT);
+            }
+        },
+        methods: {
+            ...mapActions([FETCH_POST_TITLES, FETCH_SHARES]),
+            openShareLink(url) {
+                if (!RE_URL.test(url)) {
+                    url = "http://" + url;
+                }
+                window.open(url);
+            }
+        },
         created() {
             const escListener = (evt = window.event) => evt.keyCode === 27 && this.$router.back();
             document.addEventListener('keyup', escListener);
             this.$once('hook:beforeDestroy', () => document.removeEventListener('keyup', escListener));
+
+            Promise.all([this[FETCH_POST_TITLES](), this[FETCH_SHARES]()])
+                .catch(() => this.$showToast("加载结果失败"))
+                .finally(() => this.isLoading = false);
         }
     }
 </script>
