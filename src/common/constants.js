5ee1253760a96b04b5d626642aef4cfb377ce347
ryougifujino <ryougifujino@gmail.com>
2019-06-18 11:58:36 +0800

    Add autosave to post edit

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,3 +2,4 @@ export const API_BASE_URL = "http://localhost:3000/api";
 
 export const LOCAL_KEY_FROM_USER = "LOCAL_KEY_FROM_USER";
 export const LOCAL_KEY_IS_AUTHENTICATED = "LOCAL_KEY_IS_AUTHENTICATED";
+export const LOCAL_KEY_POST_EDIT_STATE = "LOCAL_KEY_POST_EDIT_STATE";
