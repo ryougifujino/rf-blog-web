dca926d4bc98b3a063f35911d5aca679871262a1
ryougifujino <ryougifujino@gmail.com>
2019-06-25 17:34:01 +0800

    Update API_BASE_URL

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:3000/api";
+export const API_BASE_URL = process.env.NODE_ENV === 'production' ? "/api" : "http://localhost:3000/api";
 
 export const LOCAL_KEY_FROM_USER = "LOCAL_KEY_FROM_USER";
 export const LOCAL_KEY_IS_AUTHENTICATED = "LOCAL_KEY_IS_AUTHENTICATED";
